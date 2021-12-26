b174c7f8882bd5bf086aa0cbb1e712d9fd804f54
yeeun_An <an446@naver.com>
2021-12-26 21:46:58 +0900

    [feat] 모바일 구현 수정

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const btn_toggle = document.querySelector('.btn-toggle-menu');
 const list_toggle_menu = document.querySelector('.list-toggle-menu');
 
 btn_toggle.addEventListener('click', function(){
-    console.log(list_toggle_menu.style.display);
     if (list_toggle_menu.style.display ==='none'){
         list_toggle_menu.style.display ='flex';
     }else{
@@ -11,20 +10,28 @@ btn_toggle.addEventListener('click', function(){
 })
 
 //클래스로 토글
-const mode = document.querySelectorAll('#mode');
-const modeImg = document.querySelector('.btn-menu-mode img');
+const mode_go = document.querySelector('.mode-go');
+const header = document.querySelector('.header');
+const modes = document.querySelectorAll('#mode');
+const mode_img = document.querySelector('.btn-menu-mode img');
 const body = document.body;
-mode.addEventListener('click', function(){
-    if(!body.classList.contains('dark')){
-        console.log('light')
-        body.classList.add('dark');
-        // mode[1].img.src='../img/light_mode.svg'
-        modeImg.src='../img/light_mode.svg'
-        mode[0].innerHTML=`LIGHT`;
-    }else{
-        body.classList.remove('dark');
-        modeImg.src='../img/dark_mode.svg';
-        mode[0].innerHTML=`DARK`;
-        console.log('dark')
-    }
+modes.forEach(function(mode){
+    mode.addEventListener('click', function(){
+        if(!body.classList.contains('dark')){
+            header.style.backgroundColor = 'grey';
+            // header.classList.add('dark');
+            body.classList.add('dark');
+            // modes[1].img.src='../img/light_mode.svg'
+            mode_img.src='../img/light_mode.svg'
+            modes[0].classList.add('mode_go','dark');
+            modes[0].innerHTML=`LIGHT`;
+        }else{
+            header.style.backgroundColor = 'rgba(255, 255, 255, 0.884)';
+            // header.classList.remove('dark');
+            body.classList.remove('dark');
+            mode_img.src='../img/dark_mode.svg';
+            modes[0].classList.remove('mode_go','dark');
+            modes[0].innerHTML=`DARK`;
+        }
+    })
 })
